61d8693024e7b2c6096a8599822c9cef84552617
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-12-19 14:39:21 +0100

    used import instead of FQFN

--- etc/di/redis-client.php
+++ etc/di/redis-client.php
@@ -4,10 +4,12 @@ use Clue\React\Redis\Client;
 use Psr\Log\LoggerInterface;
 use React\EventLoop\LoopInterface;
 use WyriHaximus\React\Redis\WaitingClient\WaitingClient;
+use function DI\factory;
+use function DI\get;
 
 return [
-    Client::class => \DI\factory(function (LoopInterface $loop, string $dsn, LoggerInterface $logger = null) {
+    Client::class => factory(function (LoopInterface $loop, string $dsn, LoggerInterface $logger = null) {
         return WaitingClient::create($loop, $dsn, $logger);
     })
-        ->parameter('dsn', \DI\get('config.redis.dsn')),
+        ->parameter('dsn', get('config.redis.dsn')),
 ];
