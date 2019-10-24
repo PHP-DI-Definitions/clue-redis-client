51277b986721acd6499ccc09bbfd4c2d0f1e4b72
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-10-24 19:54:12 +0200

    Require ConnectorInterface

--- etc/di/redis-client.php
+++ etc/di/redis-client.php
@@ -3,10 +3,11 @@
 use Clue\React\Redis\Client;
 use Clue\React\Redis\Factory;
 use React\EventLoop\LoopInterface;
+use React\Socket\ConnectorInterface;
 
 return [
-    Client::class => \DI\factory(function (LoopInterface $loop, string $dsn) {
-        return (new Factory($loop))->createLazyClient($dsn);
+    Client::class => \DI\factory(function (LoopInterface $loop, ConnectorInterface $connector, string $dsn) {
+        return (new Factory($loop, $connector))->createLazyClient($dsn);
     })
         ->parameter('dsn', \DI\get('config.redis.dsn')),
 ];
